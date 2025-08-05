7dee6feab08c8be103c978fadc090a4b9266621d
Amir khodadi <amiramkhodadi@gmail.com>
2025-08-05 11:54:15 +0330

    gitAdd initial blog app with core features
    - Created models and applied migrations
    - Built views and templates for post listing and detail
    - Configured URLs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from blog.models import Post
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request  , 'blog/post_list.html',{'posts' : posts})
+
+
+def post_detail(request , id):
+    posts = get_object_or_404(Post , id = id , status = Post.Status.PUBLISHED)
+    # try :
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404('Post not exist')
+
+    return render(request , 'blog/post_detail.html' , {'post' : posts })
