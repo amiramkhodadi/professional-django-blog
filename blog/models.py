6304a8d59ce8d04839af776b766c1e031511d76d
Amir khodadi <amiramkhodadi@gmail.com>
2025-08-05 12:47:19 +0330

    implemented SEO-friendly URLs

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,35 @@
-from django.db import models
-from django.utils import timezone
 # from django.db.models.functions import Now
 from django.conf import settings
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
 
 # Create your models here.
 
 class PublishedManager(models.Manager):
- def get_queryset(self):
-    return (
-    super().get_queryset().filter(status=Post.Status.PUBLISHED)
-    )
-
-
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
 
 
 class Post(models.Model):
     class Status(models.TextChoices):
-        DRAFT = 'DF','Draft'
-        PUBLISHED = 'PB' , 'Published'
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete = models.CASCADE , related_name ="blog_posts")
+    slug = models.SlugField(max_length=100, unique_for_date='publish')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts")
     body = models.TextField()
-    publish = models.DateTimeField(default = timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     # publish = models.DateTimeField(db_default=Now())  ==>> moadel balaii ba in tafavot k az function khode data base estefade mikone
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2 , choices = Status , default = Status.DRAFT)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     objects = models.Manager()
-    published =PublishedManager()
-
-
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
@@ -39,6 +37,11 @@ class Post(models.Model):
             models.Index(fields=['-publish']),
         ]
 
-
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug]
+        )
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render
-from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
 from blog.models import Post
+
+
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
-    return render(request  , 'blog/post_list.html',{'posts' : posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
-def post_detail(request , id):
-    posts = get_object_or_404(Post , id = id , status = Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    posts = get_object_or_404(Post, status=Post.Status.PUBLISHED, publish__year=year, publish__month=month,
+                              publish__day=day, slug=post)
     # try :
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404('Post not exist')
 
-    return render(request , 'blog/post_detail.html' , {'post' : posts })
+    return render(request, 'blog/post_detail.html', {'post': posts})
