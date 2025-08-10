b2d2f5de4aa63c095ba6cd7895f351cd94241470
Amir khodadi <amiramkhodadi@gmail.com>
2025-08-10 09:24:25 +0330

    add some feature and debug and add simple tag v inclusion tag for posts

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,27 @@
+from django import template
+from django.db.models import Count
+
+from blog.models import Post
+
+register = template.Library()
+
+
+# @register.simple_tag(name='my_tag') ==>> dar soraty k bkhahim y name delkhah braye tag dashte bashim
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/latest_post.html')
+def show_latest_posts(count=5):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {
+        'latest_post': latest_post
+    }
+
+
+@register.simple_tag
+def get_most_commented_posts(count=3):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
